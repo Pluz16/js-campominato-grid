8178ad0616091bc1c7178c898618b72d8d57aed8
Francesco Punta <puntafrancesco@gmail.com>
2023-02-09 17:12:10 +0100

    Modifiche js, problemi con click

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
-const gridContainer = document.querySelector("grid");
+const gridContainer = document.querySelector(".grid");
 let btnPlay = document.querySelector("#play");
 
 btnPlay.addEventListener("click", function(){
+
     gridContainer.innerHTML = "";
+
     gridContainer.classList.add("grid");
     gridContainer.style.visibility = "visible";
 
-        for (let i = 1; i <= 50; i++) {
+
+
+        for (let i = 1; i <= 100; i++) {
     
             const square = document.createElement("div");
             square.classList.add("square");
