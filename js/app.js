f37e73c39fd140ac2d530fe313e52beef24f1403
Francesco Punta <puntafrancesco@gmail.com>
2023-02-09 17:23:07 +0100

    Stampo cella selezionata in console

--- js/app.js
+++ js/app.js
@@ -18,4 +18,16 @@ btnPlay.addEventListener("click", function(){
             gridContainer.append(square);
         }
 
+        const celleElements = document.querySelectorAll('.square')
+// console.log(celleElements)
+
+for (let i = 0; i < celleElements.length; i++) {
+	const square = celleElements[i]
+
+	square.addEventListener('click', function () {
+		console.log(i + 1)
+	})
+}
+
 })
+
