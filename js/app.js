0c08d737e296b093c3547bdb54bf20c6c1ea6610
Francesco Punta <puntafrancesco@gmail.com>
2023-02-09 16:54:07 +0100

    Classi, id e test js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const gridContainer = document.querySelector("grid");
+let btnPlay = document.querySelector("#play");
+
+btnPlay.addEventListener("click", function(){
+    gridContainer.innerHTML = "";
+    gridContainer.classList.add("grid");
+    gridContainer.style.visibility = "visible";
+
+        for (let i = 1; i <= 50; i++) {
+    
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.innerHTML = i;
+            gridContainer.append(square);
+        }
+
+})
